f028eabb1197cff95eddca11bb039a49bd5c8e16
GuilleMarejada <guillermo.gil.cor@gmail.com>
2024-07-12 20:39:22 +0100

    actualizamos app.py

--- src/app.py
+++ src/app.py
@@ -14,11 +14,11 @@ from models import db, User
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-db_url = os.getenv("requestBASE_URL")
+db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_requestBASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
-    app.config['SQLALCHEMY_requestBASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
